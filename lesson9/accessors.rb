7b0c2756134415ccaff2df967c493dab96b7ab19
Serge <s@shayderov.ru>
2019-08-14 21:33:15 +0300

    Accessors moudle refactoring

--- lesson9/accessors.rb
+++ lesson9/accessors.rb
@@ -5,7 +5,7 @@ module Accessors
     names.each do |name|
       var_name = "@#{name}".to_sym
       var_history = "@#{name}_history".to_sym
-      reader(name, var_name)
+      attr_reader name
       history_writer(name, var_name, var_history)
       history(name, var_history)
     end
@@ -13,22 +13,17 @@ module Accessors
 
   def strong_attr_accessor(name, type)
     var_name = "@#{name}".to_sym
-    reader(name, var_name)
+    attr_reader name
     strong_writer(name, var_name, type)
   end
 
   private
 
-  def reader(name, var_name)
-    define_method(name) { instance_variable_get(var_name) }
-  end
-
   def history_writer(name, var_name, var_history)
     define_method("#{name}=".to_sym) do |value|
       instance_variable_set(var_name, value)
       history = instance_variable_get(var_history) || []
-      history << value
-      instance_variable_set(var_history, history)
+      instance_variable_set(var_history, history << value)
     end
   end
 
